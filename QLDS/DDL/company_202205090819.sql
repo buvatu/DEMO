a62c2df7987e1d8fe5426d85d0276a6ddfff869a
TUNG Bui Van <buivantung.it@gmail.com>
2022-05-09 08:21:07 +0700

    Add data

---
+++ QLDS/DDL/company_202205090819.sql
@@ -0,0 +1,3 @@
+INSERT INTO qlds.company (company_id,company_name,address,phone_number,tax_code,director,updated_timestamp,updated_user) VALUES
+	 ('XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đường Đinh Văn Chất - Xã Hưng Đông - Thành phố Vinh - Nghệ An','','','Bùi Văn Hiện','2022-05-03 07:58:16.462','bvtung'),
+	 ('TCT_DS_VN','Tổng công ty Đường sắt Việt Nam','118 đường Lê Duẩn, Phường Cửa Nam, Quận Hoàn Kiếm, Thành phố Hà Nội','(84-24) 39425972','','','2022-05-03 17:08:47.076','bvtung');
---
+++ QLDS/DDL/engine_202205090819.sql
@@ -0,0 +1,46 @@
+INSERT INTO qlds.engine (engine_id,company_id,company_name,engine_type,updated_timestamp,updated_user) VALUES
+	 ('D18E-601','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:21:02.43','nsson'),
+	 ('D18E-602','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:40:57.722','nsson'),
+	 ('D18E-603','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:41:09.071','nsson'),
+	 ('D18E-604','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:41:20.022','nsson'),
+	 ('D18E-606','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:42:14.543','nsson'),
+	 ('D18E-607','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:42:58.297','nsson'),
+	 ('D18E-608','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:43:07.429','nsson'),
+	 ('D18E-609','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:43:21.087','nsson'),
+	 ('D18E-610','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:43:31.365','nsson'),
+	 ('D18E-611','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:43:41.899','nsson');
+INSERT INTO qlds.engine (engine_id,company_id,company_name,engine_type,updated_timestamp,updated_user) VALUES
+	 ('D18E-612','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:43:51.987','nsson'),
+	 ('D18E-613','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:44:09.202','nsson'),
+	 ('D18E-614','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:44:21.498','nsson'),
+	 ('D18E-615','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:44:32.793','nsson'),
+	 ('D18E-616','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Bỉ','2022-01-21 15:44:55.417','nsson'),
+	 ('D13E-716','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:45:07.964','nsson'),
+	 ('D13E-717','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:45:20.215','nsson'),
+	 ('D13E-718','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:45:28.934','nsson'),
+	 ('D13E-719','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:45:37.495','nsson'),
+	 ('D13E-720','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:45:48.751','nsson');
+INSERT INTO qlds.engine (engine_id,company_id,company_name,engine_type,updated_timestamp,updated_user) VALUES
+	 ('D13E-721','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:46:02.099','nsson'),
+	 ('D13E-722','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:46:11.501','nsson'),
+	 ('D13E-723','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:46:21.736','nsson'),
+	 ('D13E-724','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:46:43.695','nsson'),
+	 ('D13E-725','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-01-21 15:47:00.412','nsson'),
+	 ('D19E-901','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Trung Quốc','2022-01-21 15:47:23.892','nsson'),
+	 ('D19E-909','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Trung Quốc','2022-01-21 15:47:38.582','nsson'),
+	 ('D19E-926','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Trung Quốc','2022-01-21 15:47:53.599','nsson'),
+	 ('D9E-207','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:41:45.251','nsson'),
+	 ('D9E-211','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:41:59.984','nsson');
+INSERT INTO qlds.engine (engine_id,company_id,company_name,engine_type,updated_timestamp,updated_user) VALUES
+	 ('D9E-215','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:42:19.818','nsson'),
+	 ('D9E-218','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:42:59.093','nsson'),
+	 ('D9E-230','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:43:07.79','nsson'),
+	 ('D9E-231','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:43:16.521','nsson'),
+	 ('D9E-232','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:43:25.112','nsson'),
+	 ('D9E-234','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:43:34.583','nsson'),
+	 ('D9E-246','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:43:51.942','nsson'),
+	 ('D9E-252','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:44:17.376','nsson'),
+	 ('D9E-253','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy GE','2022-01-24 09:44:34.6','nsson'),
+	 ('D18E-605','XN_DM_VINH','Xí nghiệp Đầu máy Vinh','Đầu máy Ấn Độ','2022-05-08 21:52:35.074','bvtung');
+INSERT INTO qlds.engine (engine_id,company_id,company_name,engine_type,updated_timestamp,updated_user) VALUES
+	 ('TUNG-Test','TCT_DS_VN','Tổng công ty Đường sắt Việt Nam','Đầu máy Ấn Độ','2022-05-08 21:53:03.306','bvtung');
